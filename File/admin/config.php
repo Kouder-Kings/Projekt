722164bac03ec7ea91f90619dd8b50bc6ad2c84d
mckenzie1993 <amarczak1993@gmail.com>
2016-04-07 13:23:04 +0200

    v1.00 - Arkadiusz Marczak 07-04-2016 - Czysty OpenCart

---
+++ File/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ekup.y0.pl/admin/');
+define('HTTP_CATALOG', 'http://ekup.y0.pl/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ekup.y0.pl/admin/');
+define('HTTPS_CATALOG', 'http://ekup.y0.pl/');
+
+// DIR
+define('DIR_APPLICATION', '/virtual/ekup.y0.pl/admin/');
+define('DIR_SYSTEM', '/virtual/ekup.y0.pl/system/');
+define('DIR_LANGUAGE', '/virtual/ekup.y0.pl/admin/language/');
+define('DIR_TEMPLATE', '/virtual/ekup.y0.pl/admin/view/template/');
+define('DIR_CONFIG', '/virtual/ekup.y0.pl/system/config/');
+define('DIR_IMAGE', '/virtual/ekup.y0.pl/image/');
+define('DIR_CACHE', '/virtual/ekup.y0.pl/system/cache/');
+define('DIR_DOWNLOAD', '/virtual/ekup.y0.pl/system/download/');
+define('DIR_UPLOAD', '/virtual/ekup.y0.pl/system/upload/');
+define('DIR_LOGS', '/virtual/ekup.y0.pl/system/logs/');
+define('DIR_MODIFICATION', '/virtual/ekup.y0.pl/system/modification/');
+define('DIR_CATALOG', '/virtual/ekup.y0.pl/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.cba.pl');
+define('DB_USERNAME', 'son599');
+define('DB_PASSWORD', 'Superadmin1!');
+define('DB_DATABASE', 'ekup_y0_pl');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ File/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ekup.y0.pl/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ekup.y0.pl/');
+
+// DIR
+define('DIR_APPLICATION', '/virtual/ekup.y0.pl/catalog/');
+define('DIR_SYSTEM', '/virtual/ekup.y0.pl/system/');
+define('DIR_LANGUAGE', '/virtual/ekup.y0.pl/catalog/language/');
+define('DIR_TEMPLATE', '/virtual/ekup.y0.pl/catalog/view/theme/');
+define('DIR_CONFIG', '/virtual/ekup.y0.pl/system/config/');
+define('DIR_IMAGE', '/virtual/ekup.y0.pl/image/');
+define('DIR_CACHE', '/virtual/ekup.y0.pl/system/cache/');
+define('DIR_DOWNLOAD', '/virtual/ekup.y0.pl/system/download/');
+define('DIR_UPLOAD', '/virtual/ekup.y0.pl/system/upload/');
+define('DIR_MODIFICATION', '/virtual/ekup.y0.pl/system/modification/');
+define('DIR_LOGS', '/virtual/ekup.y0.pl/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.cba.pl');
+define('DB_USERNAME', 'son599');
+define('DB_PASSWORD', 'Superadmin1!');
+define('DB_DATABASE', 'ekup_y0_pl');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
